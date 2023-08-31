1a5fa393e27d0bb001718c5a1efb72c33f44c9f5
Андрей Остапенко <ostapenko2121@gmail.com>
2023-08-31 17:05:04 +0700

    added fade effect to project card, added services page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,33 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 export default function ProjectCard({ card }) {
+  const fadeBlockRef = useRef(null);
+
+  useEffect(() => {
+    if (!fadeBlockRef.current) return;
+
+    const viewportHeight = window.visualViewport.height;
+
+    function setOpacity() {
+      const scrolledBy = window.scrollY;
+      const opacityValue = (scrolledBy / viewportHeight) * 1.5;
+      fadeBlockRef.current.style.setProperty(
+        "opacity",
+        Math.min(opacityValue, 1)
+      );
+    }
+
+    window.addEventListener("scroll", setOpacity);
+
+    return () => window.removeEventListener("scroll", setOpacity);
+  }, [fadeBlockRef]);
 
   return (
     <div>
-      <div className="h-screen relative">
+      <div className="h-screen relative mb-4">
         <Image
           src={`/${card.imageSrc}`}
           alt={card.name}
@@ -17,7 +40,15 @@ export default function ProjectCard({ card }) {
           <p>назначение: {card.use}</p>
           <p>год: {card.year}</p>
         </div>
+        <div
+          ref={fadeBlockRef}
+          className="absolute bottom-0 w-full h-full bg-gradient-to-t from-white to-transparent"
+          style={{
+            opacity: 0,
+          }}
+        />
       </div>
+      <div className="h-[500px]">Планировка сюда</div>
     </div>
   );
 }
