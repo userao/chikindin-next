7581ee209c9717faace9ab2a80559b4ff21df450
Андрей Остапенко <ostapenko2121@gmail.com>
2023-09-12 17:50:19 +0700

    trying to fix TypeError: fetch failed when building on vercel... #4

--- src/app/(pages)/our-work/page.jsx
+++ src/app/(pages)/our-work/page.jsx
@@ -28,3 +28,5 @@ export default async function OurWork() {
     </section>
   );
 }
+
+export const dynamic = 'force-dynamic';
--- src/app/page.js
+++ src/app/page.js
@@ -23,3 +23,5 @@ export default async function Home() {
     </section>
   );
 }
+
+export const dynamic = 'force-dynamic';
