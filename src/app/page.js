e2e7f431ebfe1261aa55a9ef02c694ea12ab336c
userao <ostapenko2121@gmail.com>
2024-03-21 20:39:31 +0700

    pictures for homepage now stored locally

--- src/app/page.js
+++ src/app/page.js
@@ -6,26 +6,40 @@ import getYDImages from "@/utils/getYDImages";
 import getBase64PlaceholderUrl from "@/utils/getBase64PlaceholderUrl";
 import ReduxProvider from "@/components/ReduxProvider";
 import PageLoadedCheck from "@/components/PageLoadedCheck";
+import getFilepaths from "@/utils/getFilepaths";
+
+import fs from "node:fs/promises";
+import { getPlaiceholder } from "plaiceholder";
+import path from "node:path";
 
 export default async function Home() {
-  const allYdImages = await getYDImages();
+  // const allYdImages = await getYDImages();
 
-  const randomYdImages = projects.map((project) => {
-    const projectYdImages = getProjectImages(project.id, allYdImages);
-    const randomIndex = Math.floor(Math.random() * projectYdImages.length);
+  // const randomYdImages = projects.map((project) => {
+  //   const projectYdImages = getProjectImages(project.id, allYdImages);
+  //   const randomIndex = Math.floor(Math.random() * projectYdImages.length);
 
-    return projectYdImages[randomIndex];
-  });
+  //   return projectYdImages[randomIndex];
+  // });
 
-  const plaiceholdersPromises = randomYdImages.map((image) =>
-    getBase64PlaceholderUrl(image.file)
-  );
+  // const plaiceholdersPromises = randomYdImages.map((image) =>
+  //   getBase64PlaceholderUrl(image.file)
+  // );
 
-  const plaiceholders = await Promise.all(plaiceholdersPromises);
+  // const plaiceholders = await Promise.all(plaiceholdersPromises);
 
-  const carouselImages = randomYdImages.map((image, i) => ({
-    src: image.file,
-    base64: plaiceholders[i],
+  // const carouselImages = randomYdImages.map((image, i) => ({
+  //   src: image.file,
+  //   base64: plaiceholders[i],
+  // }));
+
+  const imagesFilepaths = await getFilepaths('public/images/homepage/');
+  const carouselImages = await Promise.all(imagesFilepaths.map(async (filepath) => {
+    const file = await fs.readFile(filepath);
+    const { base64 } = await getPlaiceholder(file);
+    const filepathWithPublicAsRoot = '/' + filepath.split('\\').filter((s) => s!=='public').join('/');
+
+    return { src: filepathWithPublicAsRoot, base64 };
   }));
 
   return (
--- src/utils/getProjectImages.js
+++ src/utils/getProjectImages.js
@@ -1,8 +1,8 @@
 export default function getProjectImages(projectId, imagesList) {
-   const projectImages = imagesList.filter((image) => {
-      const id = Number(image.path.split('/')[4]);
-      return id === projectId;
-   })
+  const projectImages = imagesList.filter((image) => {
+    const id = Number(image.path.split('/')[4]);
+    return id === projectId;
+  })
 
-   return projectImages;
+  return projectImages;
 }
