f0aca5b172ae6843769b9d3a3cb835aeed78df3a
Андрей Остапенко <ostapenko2121@gmail.com>
2024-05-03 15:15:58 +0700

    updated content and styles on services page

--- src/app/(pages)/services/page.jsx
+++ src/app/(pages)/services/page.jsx
@@ -13,47 +13,89 @@ export default function Services() {
     <main>
       <TextSpinner text="услуги" radius={150} color={"brand-primary-400"} />
       <section className="flex flex-col items-center">
-        <h2 className="uppercase mb-6">Пакеты услуг</h2>
-        <div className="flex gap-8 w-[80%]">
-          <div>
-            <h3>Пакет C</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-              nulla corrupti necessitatibus aliquid saepe totam odio quo fuga
-              suscipit quam, nemo ipsam ex dolores beatae earum voluptatibus
-              nisi minus, delectus voluptas consequuntur quis sed. Quisquam,
-              architecto rem! Possimus quaerat, impedit consectetur error
-              asperiores nostrum eaque? Aut possimus dolores sequi iusto.
+        <h2 className="uppercase my-6">Пакеты услуг</h2>
+        <div className="flex gap-10 mx-6 flex-wrap justify-center">
+          <div className="w-[19rem] relative">
+            <h3 className="uppercase">
+              Пакет{" "}
+              <span className="text-brand-primary-400 text-2xl relative -bottom-[2.5px]">
+                C
+              </span>
+            </h3>
+            <ul className="list-services pl-4">
+              <li className="py-2">Обмерный план</li>
+              <li className="py-2">Планировочное решение</li>
+              <li className="py-2">Альбом технического задания</li>
+              <li className="py-2">План осветительных приборов</li>
+              <li className="py-2 pb-8">План электрики</li>
+            </ul>
+            <p className="text-right absolute w-full bottom-0">
+              1800 р/м<span style={{ fontVariantPosition: "super" }}>2</span>
             </p>
           </div>
-          <div>
-            <h3>Пакет H</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-              earum quidem hic quod magnam vitae commodi exercitationem in quo
-              voluptate reiciendis explicabo modi suscipit non consequuntur qui
-              quos fugiat possimus quae, vero alias eveniet dignissimos? Magni
-              perferendis repudiandae modi accusamus, nostrum voluptatum,
-              nesciunt molestias pariatur sunt minus eius. Minima, unde.
+          <div className="w-[19rem] relative">
+            <h3 className="uppercase">
+              Пакет{" "}
+              <span className="text-brand-primary-400 text-2xl relative -bottom-[2.5px]">
+                H
+              </span>
+            </h3>
+            <ul className="list-services pl-4">
+              <li className="py-2">Обмерный план</li>
+              <li className="py-2">Планировочное решение</li>
+              <li className="py-2">Альбом технического задания</li>
+              <li className="py-2">Концептуальный мудборд</li>
+              <li className="py-2 pb-8">Проектная документация</li>
+            </ul>
+            <p className="text-right absolute w-full bottom-0">
+              2000 р/м<span style={{ fontVariantPosition: "super" }}>2</span>
             </p>
           </div>
-          <div>
-            <h3>Пакет CH</h3>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
-              dignissimos velit dolore repudiandae porro eum adipisci labore
-              magni voluptatem nesciunt. At sit, labore nisi id obcaecati
-              doloremque tenetur ut! Sapiente cum explicabo architecto
-              laboriosam ut, molestias vero, repellendus adipisci aut at eveniet
-              voluptas id modi, doloribus tempora? Ipsam, ut deleniti?
+          <div className="w-[19rem] relative">
+            <h3 className="uppercase">
+              Пакет{" "}
+              <span className="text-brand-primary-400 text-2xl relative -bottom-[2.5px]">
+                CH
+              </span>
+            </h3>
+            <ul className="list-services pl-4">
+              <li className="py-2">Обмерный план</li>
+              <li className="py-2">Планировочное решение</li>
+              <li className="py-2">Альбом технического задания</li>
+              <li className="py-2">Визуализация</li>
+              <li className="py-2">Проектная документация</li>
+              <li className="py-2 pb-8">Базовый подбор материалов</li>
+            </ul>
+            <p className="text-right absolute w-full bottom-0">
+              2500 р/м<span style={{ fontVariantPosition: "super" }}>2</span>
+            </p>
+          </div>
+          <div className="w-[19rem] relative">
+            <h3 className="uppercase">
+              Пакет{" "}
+              <span className="text-brand-primary-400 text-2xl relative -bottom-[2.5px]">
+                CH<span className="text-[0.6rem]">de luxe</span>
+              </span>
+            </h3>
+            <ul className="list-services pl-4">
+              <li className="py-2">Обмерный план</li>
+              <li className="py-2">Планировочное решение</li>
+              <li className="py-2">Альбом технического задания</li>
+              <li className="py-2">Визуализация</li>
+              <li className="py-2">Проектная документация</li>
+              <li className="py-2">Полная комплектация</li>
+              <li className="py-2 pb-8">1 месяц авторского надзора</li>
+            </ul>
+            <p className="text-right absolute w-full bottom-0">
+              3000 р/м<span style={{ fontVariantPosition: "super" }}>2</span>
             </p>
           </div>
         </div>
       </section>
       <section className="flex justify-center">
         <div className="max-w-[700px] mx-4 my-8">
-          <h2 className="uppercase mb-6">Этапы работ</h2>
-          <ol className="services-list">
+          <h2 className="uppercase my-6">Этапы работ</h2>
+          <ol className="list-services-numbered">
             <li className="mb-6">
               <h3 className="uppercase pl-10">
                 Составление технического задания на проектирование
